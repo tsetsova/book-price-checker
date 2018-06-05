74f4181239685d094170dacf81561078c2ce45b0
Ina Tsetsova <inatsetsova@gmail.com>
2018-06-05 13:38:13 +0100

    Compare current price with a hardcoded fixed price

--- main.rb
+++ main.rb
@@ -5,27 +5,27 @@ class BookPriceChecker
 
   def initialize
     @url = read_url
+    @current_price = get_price
+    @desired_price = 1.99
   end
   
-  def get_body
-    response = Faraday.get(@url)
-    response.body
+  def get_response_body
+    Faraday.get(@url).body
   end
 
   def get_price
-    scrape_for_price(Nokogiri::HTML(get_body))
+    scrape_for_price(Nokogiri::HTML(get_response_body)).to_f
   end
 
-  #set desired price
+  def cheap_enough?
+    @current_price <= @desired_price
+  end
 
-  #compare_price with stored price
-
+  private
 
   def read_url
     file = File.readlines('urls.txt').first.strip
   end 
-
-  private 
 
   def scrape_for_price(html)
     match = /£(\d\.\d+)/.match(html.at_css('tr.kindle-price td.a-color-price').text)
@@ -35,6 +35,6 @@ end
 
 book_price_checker = BookPriceChecker.new
 
-puts book_price_checker.read_url
-
 puts book_price_checker.get_price
+
+puts book_price_checker.cheap_enough?
